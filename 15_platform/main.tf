1d7f581ed8c6567522e729fed6a8a897403f107a
Ali <aly.shaheen14@gmail.com>
2024-10-24 12:39:04 +0300

    add platform-15 layer

---
+++ 15_platform/main.tf
@@ -0,0 +1,84 @@
+locals {
+  cluster_name = "terraform-k8s-demo"
+}
+
+#  configue dns to point to the ingress had recently created
+data "kubernetes_service" "ingress_service" {
+  metadata {
+    name      = "ingress-nginx-controller"
+    namespace = "ingress-nginx"
+  }
+}
+
+data "aws_route53_zone" "default" {
+  name         = "k8s.demo"
+  private_zone = true
+}
+
+resource "aws_route53_record" "ingress_record" {
+  zone_id = data.aws_route53_zone.default.zone_id
+  name    = "app.k8s.demo"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [data.kubernetes_service.ingress_service.status.0.load_balancer.0.ingress.0.hostname]
+}
+
+# configure cert-manager to create tls certificate
+resource "kubernetes_manifest" "cert_issuer" {
+  manifest = yamldecode(<<YAML
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    server: https://acme-v02.api.letsencrypt.org/directory
+    email: ali@intx.io
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    solvers:
+      - http01:
+          ingress:
+            ingressClassName: nginx
+    YAML
+  )
+  depends_on = [
+    aws_route53_record.ingress_record
+  ]
+}
+
+# configure the secret manager to enable eso to fitch secrets from aws parameter store.
+data "aws_caller_identity" "current" {}
+
+resource "kubernetes_service_account" "secret_store" {
+  metadata {
+    name      = "secret-store"
+    namespace = "external-secrets"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = "arn:aws:iam:${data.aws_caller_identity.current.account_id}:role/secret-store"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "cluster_secret_store" {
+  manifest = yamldecode(<<YAML
+apiVersion: external-secrets.io/v1alpha1
+kind: ClusterSecretStore
+metadata:
+  name: aws-store
+spec:
+  provider:
+    aws:
+      service: ParameterStore
+      region: eu-central-1
+      auth:
+        jwt:
+          serviceAccountRef:
+            name: secret-store
+            namespace: external-secret
+    YAML
+  )
+  depends_on = [
+    kubernetes_service_account.secret_store
+  ]
+}
---
+++ 15_platform/providers.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.72.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.16.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-k8s-demo"
+    key    = "aws/15_platform"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
