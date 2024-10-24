af3111fd2764b2c73dd6c5ce0e366a4b7d238f9c
Ali <aly.shaheen14@gmail.com>
2024-10-24 12:36:05 +0300

    fix cluster permission issue

--- 00_foundations/main.tf
+++ 00_foundations/main.tf
@@ -33,7 +33,8 @@ module "cluster" {
   cluster_name    = local.cluster_name
   cluster_version = "1.31"
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
